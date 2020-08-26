2c56972b9b2d6726018c5850a2057a44552b4d65
Camesi <camesi.jean@gmail.com>
2020-08-26 22:12:14 +0200

    ..

--- src/js/script.js
+++ src/js/script.js
@@ -22,7 +22,7 @@ var loadPokemon = function() {
 	
 	for(var k in pokemonData.pokemon) {
 		var pokemon_node = document.createElement("div");
-		pokemon_node.appendChild(document.createTextNode(pokemonData.pokemon[k].name));
+		pokemon_node.innerHTML = document.createTextNode(pokemonData.pokemon[k].name);
 		
 		list_node.appendChild(pokemon_node);
 	}
