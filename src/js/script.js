bb6beb85cbb6e2114663134ccd2f6440b4d7b8c1
Camesi <camesi.jean@gmail.com>
2020-08-29 22:06:00 +0200

    ...

--- src/js/script.js
+++ src/js/script.js
@@ -274,6 +274,9 @@ function getAttacks() {
 		leftside_node.innerHTML += "<span>Temps par cycle:</span>";
 		leftside_node.innerHTML += "<span>Dmg sur 90s:</span>";
 		
+		var midside_node = document.createElement("div");
+		midside_node.innerHTML = "V";
+		
 		var rightside_node = document.createElement("div");
 		var namecontainer_node = document.createElement("div");
 		var img = document.createElement("img");
@@ -288,6 +291,7 @@ function getAttacks() {
 		rightside_node.innerHTML += "<span>" + Math.round((rank_combi[k].averageDmg + Number.EPSILON) * 100) / 100 + "</span>";
 		
 		attack_node.appendChild(leftside_node);
+		attack_node.appendChild(midside_node);
 		attack_node.appendChild(rightside_node);
 		
 		attacks_node.appendChild(attack_node);
